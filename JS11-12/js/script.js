c659492394a20ca349d7f1b2de166f83f17440e4
Skaliarium <serggora12@gmail.com>
2017-08-06 16:06:43 +0300

    hw js11-12 2

--- JS11-12/js/script.js
+++ JS11-12/js/script.js
@@ -22,34 +22,9 @@ content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm
   var content = tmpl (html, {data: info});
   $('body').append(content);
 
+  $('.carusel').carusel ({
+
+  });
 
 
-
-
-    var leftUIEl = $(".carousel-arrow-left");
-    var rightUIEl = $(".carousel-arrow-right");
-    var elementsList = $(".carousel-list");
-
-
-
-    var pixelsOffset = 175;
-    var currentLeftValue = 0;
-    var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
-    var maximumOffset = 0;
-
-
-    leftUIEl.click(function() {
-        if (currentLeftValue != maximumOffset) {
-            currentLeftValue += 175;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
-
-    rightUIEl.click(function() {
-        if (currentLeftValue != minimumOffset) {
-            currentLeftValue -= 175;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
 });
