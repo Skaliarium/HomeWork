ccb8fc963df17e1d8c54b0a90cc3ff069d2d6b53
Skaliarium <serggora12@gmail.com>
2017-08-06 15:26:57 +0300

    hw js11-12

---
+++ JS11-12/js/script.js
@@ -0,0 +1,55 @@
+$ (function () {
+
+  $('a.modalimg').modalimg ({
+    overlayColor: 'red'
+  });
+
+  var html = $('#item_tmpl').html();
+  var info = [
+    {
+    title: "article1",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+  title: "article2",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},
+{
+title: "article3",
+content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
+];
+  var content = tmpl (html, {data: info});
+  $('body').append(content);
+
+
+
+
+
+    var leftUIEl = $(".carousel-arrow-left");
+    var rightUIEl = $(".carousel-arrow-right");
+    var elementsList = $(".carousel-list");
+
+
+
+    var pixelsOffset = 175;
+    var currentLeftValue = 0;
+    var elementsCount = elementsList.find('li').length;
+    var minimumOffset = - ((elementsCount - 5) * pixelsOffset);
+    var maximumOffset = 0;
+
+
+    leftUIEl.click(function() {
+        if (currentLeftValue != maximumOffset) {
+            currentLeftValue += 175;
+            elementsList.animate({ left : currentLeftValue + "px"}, 500);
+        }
+    });
+
+    rightUIEl.click(function() {
+        if (currentLeftValue != minimumOffset) {
+            currentLeftValue -= 175;
+            elementsList.animate({ left : currentLeftValue + "px"}, 500);
+        }
+    });
+});
---
+++ M12-JS/js/script.js
@@ -0,0 +1,26 @@
+$ (function () {
+
+  $('a.modalimg').modalimg ({
+    overlayColor: 'red'
+  });
+
+  var html = $('#item_tmpl').html();
+  var info = [
+    {
+    title: "article1",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+  title: "article2",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},
+{
+title: "article3",
+content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
+];
+  var content = tmpl (html, {data: info});
+  $('body').append(content);
+
+
+});
